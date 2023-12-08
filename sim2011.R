f06fd8d61df17c10e97d2ab0812a2fe48d349bef
Taeseok Park <tspark@tamu.edu>
2023-12-08 13:19:02 -0600

    Make implementation code

---
+++ sim2011.R
@@ -0,0 +1,6 @@
+sim2011 <- function(n, beta, sigma, phi, M=100, max.iter=50, eps=1e-09, B=2000, method=c("FI", "CFI")){
+  if method=="FI" {
+    source("myPFI.R")
+    res <- function(n=n, beta=beta, sigma=sigma, phi=phi, M=M, max.iter=max.iter, eps=eps, B=B)
+  }
+}
